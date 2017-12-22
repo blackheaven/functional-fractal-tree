189d9ff6590c1482362cbba7b24f05e5bf060ffb
Gautier DI FOLCO <gautier.difolco@gmail.com>
2017-12-22 18:03:09 +0100

    Add splitSpace to do the vertical split

--- src/FractalTree.hs
+++ src/FractalTree.hs
@@ -3,6 +3,8 @@ module FractalTree
     ) where
 
 import Test.QuickCheck
+import Data.Monoid
+import Data.List(group)
 
 data Pixel = Space | One deriving (Eq, Show)
 instance Arbitrary Pixel where
@@ -21,6 +23,16 @@ render (Row xs) = map convert xs
   where convert x = case x of
                       Space -> '_'
                       One   -> '1'
+
+-- | Split the drawing space horizontally in the middle and apply each
+-- function on each part
+-- 
+-- prop> x >= 0 ==> length (splitSpace x (`replicate` 'T') (`replicate` 'B')) == x
+-- prop> x >= 0 ==> length (group (splitSpace x (`replicate` 'T') (`replicate` 'B'))) <= 2
+-- prop> x >= 2 ==> map head (group (splitSpace x (`replicate` 'T') (`replicate` 'B'))) == ['T', 'B']
+-- prop> x >= 2 ==> let [a,b] = map length (group (splitSpace x (`replicate` 'T') (`replicate` 'B'))) in elem (b-a) [0,1]
+splitSpace :: Monoid a => Int -> (Int -> a) -> (Int -> a) -> a
+splitSpace s t b = t (div s 2) <> b (div (s+1) 2)
 
 -- | generate the lines to be displayed
 -- 
